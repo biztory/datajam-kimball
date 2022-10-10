eb8367b4e51b0215609e83c1594e691bbcdec920
ChrisBiztory <chris.beagley@biztory.be>
2022-10-10 12:26:44 +0100

    specified columns to be returned for dim people

--- models/dimensions/dim_people.sql
+++ models/dimensions/dim_people.sql
@@ -9,7 +9,28 @@ rm_users as (
     select * from {{ref('stg_users')}}
 ),
 final as (
-    select * from biztory_team bt
+    select
+    bt.full_name as member,
+    bt.email_address as member_email,
+    bt.country as member_country,
+    bt.team as member_team,
+    bt.role as member_role, 
+    bt.reports_to as member_reports_to,
+    bt.biztory_branch as member_biztory_branch,
+    bt.start_date as member_start_date,
+    bt.end_date as member_end_date,
+    bt.status as member_status,
+    bt.cronos_login as member_cronos_login,
+    bt.phone_number as member_phone_number,
+    bt.discipline as member_discipline,
+    bt.birthday as member_birthday,
+    bt.approver as member_approver,
+    rm.LAST_LOGIN_TIME as rm_last_login_time,
+    rm.has_login as rm_has_login,
+    rm.account_owner as rm_account_owner,
+    rm.billability_target as member_target_billable_rate,
+    rm.billable as member_is_billable
+    from biztory_team bt
     LEFT JOIN hubspot h ON lower(split(bt.email_address, '@')[0]::string) = lower(split(h.email, '@')[0]::string)
     LEFT JOIN rm_users rm on lower(split(bt.email_address, '@')[0]::string) = lower(split(rm.email, '@')[0]::string)
 )
